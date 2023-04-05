33482ebf605b7f8b6d1d323555d75c1bff34a7b4
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-04-05 20:59:25 -0300

    Create base.py

---
+++ base.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+
+rec = sr.Recognizer()
+print(sr.Microphone().list_microphone_names())
+with sr.Microphone(device_index=0) as mic:
+    rec.adjust_for_ambient_noise(mic)
+    print("Pode falar que eu vou gravar")
+    audio = rec.listen(mic)
+    texto = rec.recognize_google(audio, language="pt-BR")
+    print(texto)
