db8cf6c4161c20f4f300be77a445f4a19dfe8286
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-04-06 08:01:31 -0300

    Update base.py

--- base.py
+++ base.py
@@ -1,13 +1,18 @@
 import speech_recognition as sr
+from art import *
 
 rec = sr.Recognizer()
-print(sr.Microphone().list_microphone_names())
+#print(sr.Microphone().list_microphone_names())
 with sr.Microphone(device_index=0) as mic:
     rec.adjust_for_ambient_noise(mic)
-    print("Pode falar que eu vou gravar")
+    #print("Pode falar que eu vou gravar")
+    tprint("Bem vindo")
+    tprint("DANIELDPEREIRA")
     audio = rec.listen(mic)
     texto = rec.recognize_google(audio, language="pt-BR")
     print(texto)
 
     if texto == "Olá" or "olá":
-        print(1+1)
+        print("Olá capitão")
+
+input()
