cfbfae48dd054ec4c6a2eb3c4171e4651410ecd8
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-04-10 11:15:29 -0300

    Update hora.py

--- hora.py
+++ hora.py
@@ -3,8 +3,8 @@ import datetime
 hora = datetime.datetime.now()
 
 hora = str(hora).split()
-
 hora = hora[1]
 hora = hora.split(":")
+hora = hora[0] + " horas e " + hora[1] + " minutos"
 
 print(hora)
