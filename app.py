5a5c0c99b7f0ee8a2bc307ddd5dad26ce370805e
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-04-10 12:35:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,12 @@ with sr.Microphone(device_index=0) as mic:
     rec.adjust_for_ambient_noise(mic)
     #print("Pode falar que eu vou gravar")
 
+    '''
     engine.say("Olá Daniel! Eu sou o seu sistema de assistência personalizado e estou aqui para te ajudar")
     engine.runAndWait()
     engine.stop()
-    
+    '''
+
     tprint("WELCOME MR. DANIEL")
     audio = rec.listen(mic)
     texto = rec.recognize_google(audio, language="pt-BR")
