e55751f2c9b0d14ca3de3f4681b903021fade733
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-04-13 16:57:56 -0300

    Update pesquisar.py

--- assets/pesquisar.py
+++ assets/pesquisar.py
@@ -1,5 +1,6 @@
 import os
 
-url = "https://www.github.com"
+pesquisa = "homem aranha"
+src = "https://www.google.com/search?q={pesquisa}"
 
-os.system(f"start {url}")
+os.system(f"start https://www.google.com/search?q="+pesquisa)
