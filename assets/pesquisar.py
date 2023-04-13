cd90b1283f3c6d13202b3feecc021fd41a5f60a8
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-04-13 20:02:20 -0300

    Update pesquisar.py

--- assets/pesquisar.py
+++ assets/pesquisar.py
@@ -2,12 +2,8 @@ import os
 
 pesquisa = "homem aranha sem volta para casa"
 pesquisa = pesquisa.split()
-print(pesquisa)
-
 pesquisa = "+".join(pesquisa) 
 
-print(pesquisa)
+url = "https://www.google.com/search?q="+str(pesquisa)
 
-src = "https://www.google.com/search?q="+str(pesquisa)
-
-os.system(f"start {src}")
+os.system(f"start {url}")
