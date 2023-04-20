ed4d402e28b732bbb945dc5ff53382e7db734df3
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-04-20 08:04:35 -0300

    Update DEISE.py

--- DEISE.py
+++ DEISE.py
@@ -20,11 +20,11 @@ engine.setProperty('voice', voice[0].id) #changing voice to index 1 for female v
 
 tprint("Welcome   Mr.   Daniel")
 
-"""
+
 engine.say(f"Olá {UserName}! Eu sou a {SysName}, sua assistente pessoal e estou aqui para te ajudar")
 engine.runAndWait()
 engine.stop()
-"""
+
 
 def DEISE_():
 
