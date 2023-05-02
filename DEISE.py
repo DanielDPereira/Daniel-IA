1b96b299c9d9cdd91f950826abd62a341517ba9a
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-05-02 12:11:04 -0300

    Update DEISE.py

--- DEISE.py
+++ DEISE.py
@@ -105,7 +105,7 @@ def DEISE_():
                  print("%s:\t\t\t%f" % "Brawlhalla", "1")
                  print("--------------------")
 
-            def TabelaJogos()    
+            TabelaJogos()    
                 
             if texto == "vamos jogar":
 
